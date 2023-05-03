01a8a66aaf03e4a57f9e3063cef7c01624483144
fmaia85 <felipe.maia@ifce.edu.br>
2023-05-03 12:26:25 -0300

    Resolvidos bugs com leiGetPos e leiDelPos.

--- lista_encadeada.c
+++ lista_encadeada.c
@@ -1,56 +1,79 @@
 #include "lista_encadeada.h"
 #include <stdlib.h>
-#include <stdio.h>
-
-lista_enc_int* leiStart(int cabeca){
-    lista_enc_int* head;
-    head = (lista_enc_int*) malloc(sizeof(lista_enc_int));
+lista_enc_int *leiStart(int cabeca)
+{
+    lista_enc_int *head;
+    head = (lista_enc_int *)malloc(sizeof(lista_enc_int));
     head->valor = cabeca;
     head->prox = 0;
     return head;
 }
 
-void leiDestruct(lista_enc_int* lista){
-    if(lista->prox != 0){
+void leiDestruct(lista_enc_int *lista)
+{
+    if (lista->prox != 0)
+    {
         leiDestruct(lista->prox);
     }
     free(lista);
 }
 
-void leiAddLast(lista_enc_int* lista, int valor){
-    if(lista->prox != 0){
+void leiAddLast(lista_enc_int *lista, int valor)
+{
+    if (lista->prox != 0)
+    {
         leiAddLast(lista->prox, valor);
-    } else {
+    }
+    else
+    {
         lista->prox = leiStart(valor);
     }
 }
 
-void leiAddPos(lista_enc_int* lista, int pos, int valor){
-    if(pos <= 0){
-        lista_enc_int* node = leiStart(lista->valor);
+void leiAddPos(lista_enc_int *lista, int pos, int valor)
+{
+    if (pos <= 0)
+    {
+        lista_enc_int *node = leiStart(lista->valor);
         node->prox = lista->prox;
         lista->prox = node;
-        lista->valor = valor;      
-    } else {
-        leiAddPos(lista->prox, pos-1, valor);
+        lista->valor = valor;
     }
-
+    else
+    {
+        leiAddPos(lista->prox, pos - 1, valor);
+    }
 }
 
-int leiDelPos(lista_enc_int* lista, int pos){
-    printf("Chamou o metodo leiDelPos.\n");
-    return 0;
+int leiDelPos(lista_enc_int *lista, int pos)
+{
+    if (pos <= 0)
+    {
+        lista_enc_int *target = lista->prox;
+        int valor = lista->valor;
+        lista->valor = target->valor;
+        lista->prox = target->prox;
+        free(target);
+        return valor;
+    }
+    return leiDelPos(lista->prox, pos - 1);
 }
 
-int leiGetPos(lista_enc_int* lista, int pos){
-    printf("Chamou o metodo leiGetPos.\n");
-    return 0;
+int leiGetPos(lista_enc_int *lista, int pos)
+{
+    if (pos <= 0)
+    {
+        int valor = lista->valor;
+        return valor;
+    }
+    return leiGetPos(lista->prox, pos-1);
 }
 
-int leiGetTam(lista_enc_int* lista){
-    if(lista->prox == 0){
+int leiGetTam(lista_enc_int *lista)
+{
+    if (lista->prox == 0)
+    {
         return 1;
     }
     return leiGetTam(lista->prox) + 1;
 }
-
--- main.c
+++ main.c
@@ -7,16 +7,20 @@ int main(int argc, char const *argv[])
 {
     lista_enc_int* lista = leiStart(25);
     
-    leiAddLast(lista, 35);
+    leiAddLast(lista, 44);
     leiAddLast(lista, 55);
 
-    leiAddPos(lista, 0, 15);
-    leiAddPos(lista, 2, 30);
+    leiAddPos(lista, 0, 5);
 
-    leiDelPos(0, 0);
-    leiGetPos(0, 0);
+    leiDelPos(lista, 2);
     int tam = leiGetTam(lista);
     printf("Tamanho da lista: %d.\n", tam);
+
+   
+    for(int i = 0; i < tam; i++){
+        int v = leiGetPos(lista, i);
+        printf("[%d] %d; ", i, v);
+    }
 
     leiDestruct(lista);
 
